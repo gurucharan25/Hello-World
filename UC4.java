4389b411712fd0d16cedacb661a8b96bbc5c50e8
gurucharan25 <thonangigurucharan@gmail.com>
2026-04-22 21:36:21 +0530

    Add files via upload

---
+++ UC4.java
@@ -0,0 +1,19 @@
+public class UC4 {
+
+    public static void main(String[] args) {
+
+        String names;
+
+
+        if (args.length > 0) {
+
+            names = String.join(", ", args);
+        } else {
+
+            names = "World";
+        }
+
+
+        System.out.println("Hello, " + names + "!");
+    }
+}
